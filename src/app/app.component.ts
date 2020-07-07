aba530baeacc603e809f8149e9b57f6a3e3065a9
ashish9342 <ashish9342@gmail.com>
2020-07-07 15:07:19 +0300

    collapse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,128 @@
-import { Component, VERSION } from '@angular/core';
+// app.component.ts
+import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+
+  isTableExpanded = false;
+
+  STUDENTS_DATA = [
+    {
+      "id": 1,
+      "name": "Abby Jaskolski ",
+      "age": 21,
+      "address": 1.0079,
+      "isExpanded": false,
+      "subjects": [
+        {
+          "name": "Bio",
+          "type": "Medical",
+          "grade": "A",
+          "id": 1,
+          "age": 21,
+          "address": 1.0079,
+          "some": "Action",
+        },
+        {
+          "name": "Chemistry",
+          "type": "Medical",
+          "grade": "A",
+          "id": 1,
+          "age": 21,
+          "address": 1.0079,
+          "some": "Action",
+        },
+        {
+          "name": "Physics",
+          "type": "Medical",
+          "grade": "A",
+          "id": 1,
+          "age": 21,
+          "address": 1.0079,
+          "some": "Action",
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Jabari Fritsch",
+      "age": 20,
+      "address": 1.0079,
+      "isExpanded": false,
+      "subjects": [
+        {
+          "name": "Bio",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Chemistry",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Physics",
+          "type": "Medical",
+          "grade": "A"
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Maybell Simonis",
+      "age": 21,
+      "address": 1.0079,
+      "isExpanded": false,
+      "subjects": [
+        {
+          "name": "Bio",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Chemistry",
+          "type": "Medical",
+          "grade": "A"
+        },
+        {
+          "name": "Physics",
+          "type": "Medical",
+          "grade": "A"
+        }
+      ]
+    }
+  ];
+
+
+  dataStudentsList = new MatTableDataSource();
+  displayedStudentsColumnsList: string[] = ['id', 'name', 'age', 'address', 'actions'];
+
+
+  ngOnInit() {
+    this.dataStudentsList.data = this.STUDENTS_DATA;
+  }
+
+  // Toggel Rows
+  toggleTableRows() {
+    this.isTableExpanded = !this.isTableExpanded;
+
+    this.dataStudentsList.data.forEach((row: any) => {
+      row.isExpanded = this.isTableExpanded;
+    })
+  }
+
 }
